c87cb612bb65d7713092e0b50d533d9f9572d6b7
Constikri <constantin.krischke@stud.hshl.de>
2020-03-21 18:56:54 +0100

    added request parsing for leader board

--- routes/leaders.js
+++ routes/leaders.js
@@ -1,7 +1,20 @@
+const models = require('../models');
+
 module.exports = app => {
 
-    app.get('/leaders', function (req, res){
-        res.send(__filename)
+    app.get('/leaders/:rank', function (req, res){
+        models['user'].findAll({
+            where: {
+                rank: req.params.rank
+            },
+            attributes: ['username', 'score', 'country'],
+            order:[
+                sequelize.fn('max', sequelize.col('score'), 'DESC')
+            ],
+            limit: 20            
+        }).then(leaders => {
+            res.send(JSON.stringify(leaders));
+        });
     });
 
 };
