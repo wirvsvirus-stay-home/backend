5321fb3577a4f86b5021ace726a3be12873546d9
pklink <pklink@pro-vision.de>
2020-03-21 20:09:59 +0100

    `GET /i18n/:countryCode` hinzugefügt

--- models/I18n.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('i18n', {
-        language: DataTypes.STRING(8),
-        key: DataTypes.STRING,
-        text: DataTypes.TEXT
-    })
-};
--- routes/i18n.js
+++ routes/i18n.js
@@ -1,12 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = app => {
 
-    app.get('/i18n/:CountryCode', function (req, res){
-        res.send(__filename)
-        // {
-        //     "welcome": "Hallo :username:",
-        //     "save": "Speichern",
-        //     // ....
-        // }
-    });
+    app.get('/i18n/:countryCode', (req, res) => {
+        const { countryCode } = req.params;
+
+        // Pruefen ob countryCode angegeben wurde
+        if (countryCode == null) {
+            res.status(404).send({ status: 404, message: "Not Found" });
+            return;
+        }
+
+        // Pfad zu Sprachdatei ermittlen
+        const languageFile = path.join(__dirname, '..', 'i18n', path.basename(countryCode)) + '.json';
+
+        // Datei einlesen und zurückgeben
+        fs.readFile(languageFile, 'utf8', (err, data) => {
+            // Wenn Datei nicht eingelesen werden konnt, 404 zurückgeben
+            if (err) {
+                res.status(404).send({ status: 404, message: "Not Found" });
+                return;
+            }
+
+            res.type('application/json').send(data);
+        });
+    })
 
 };
